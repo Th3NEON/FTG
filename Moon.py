41bd8599cb231b6e6c7933c9d43c4069b552a790
begi717 <61600291+begi717@users.noreply.github.com>
2020-08-09 11:34:45 +0500

    Update Moon.py

--- Moon.py
+++ Moon.py
@@ -9,7 +9,7 @@ from collections import deque
 async def _(event):
 	if event.fwd_from:
 		return
-	deq = deque(list("ðŸŒ—ðŸŒ˜ðŸŒ‘ðŸŒ’ðŸŒ“ðŸŒ”ðŸŒ•ðŸŒ–"))
+	deq = deque(list("🌚🌝🌚🌝🌚🌝🌚🌝🌚🌝🌚🌝🌚🌝🌚🌝🌚🌝🌚🌝🌚🌝🌚🌝🌚🌝🌚🌕🌖🌗🌘🌑🌒🌓🌔🌝"))
 	for _ in range(32):
 		await asyncio.sleep(0.1)
 		await event.edit("".join(deq))
