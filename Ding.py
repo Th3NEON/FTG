c8b5c2fe806761238aadadd78860a04b48cda1f6
begi717 <61600291+begi717@users.noreply.github.com>
2020-08-09 15:43:32 +0500

    Create Ding.py

---
+++ Ding.py
@@ -0,0 +1,31 @@
+
+# Edited for FTg by demenkop
+
+
+from telethon import events
+import asyncio
+
+@borg.on(events.NewMessage(pattern=r"\.ding", outgoing=True))
+
+async def _(event):
+    if event.fwd_from:
+        return
+
+    animation_interval = 0.3
+    animation_ttl = range(0, 10)
+    input_str = event.pattern_match.group(1)
+    animation_chars = [
+            "🔴⬛⬛⬜⬜\n⬜⬜⬜⬜⬜\n⬜⬜⬜⬜⬜",
+            "⬜⬜⬛⬜⬜\n⬜⬛⬜⬜⬜\n🔴⬜⬜⬜⬜",
+            "⬜⬜⬛⬜⬜\n⬜⬜⬛⬜⬜\n⬜⬜🔴⬜⬜",
+            "⬜⬜⬛⬜⬜\n⬜⬜⬜⬛⬜\n⬜⬜⬜⬜🔴",
+            "⬜⬜⬛⬛🔴\n⬜⬜⬜⬜⬜\n⬜⬜⬜⬜⬜",    
+            "⬜⬜⬛⬜⬜\n⬜⬜⬜⬛⬜\n⬜⬜⬜⬜🔴",
+            "⬜⬜⬛⬜⬜\n⬜⬜⬛⬜⬜\n⬜⬜🔴⬜⬜",
+            "⬜⬜⬛⬜⬜\n⬜⬛⬜⬜⬜\n🔴⬜⬜⬜⬜",
+            "🔴⬛⬛⬜⬜\n⬜⬜⬜⬜⬜\n⬜⬜⬜⬜⬜",
+            "⬜⬜⬜⬜⬜\n⬜ [Demenkop](http://t.me/demenkop) ⬜\n⬜⬜⬜⬜⬜"
+ ]
+    for i in animation_ttl:
+        await asyncio.sleep(animation_interval)
+        await event.edit(animation_chars[i % 10])
