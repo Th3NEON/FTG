62de1544f378855478808870aac510411509bf5e
begi717 <61600291+begi717@users.noreply.github.com>
2020-08-11 16:44:24 +0500

    Update RemoveBG.py

--- RemoveBG.py
+++ RemoveBG.py
@@ -5,6 +5,7 @@
 
 # (c) Shrimadhav U K - UniBorg
 # Thanks to Prakasaka for porting.
+# Thanks to laciamemeframe for optimization and translate
 
 import io
 import os
@@ -14,41 +15,36 @@ from telethon.tl.types import MessageMediaPhoto
 from userbot import CMD_HELP, REM_BG_API_KEY, TEMP_DOWNLOAD_DIRECTORY
 
 
-@register(outgoing=True, pattern="^.remove(?: |$)(.*)")
+@register(outgoing=True, pattern="^.remove?: |$)(.*)")
 async def kbg(remob):
-    """ For .rbg command, Remove Image Background. """
-    if REM_BG_API_KEY is None:
-        await remob.edit(
-            "`Error: Remove.BG API key missing! Add it to environment vars or config.env.`"
-        )
-        return
+    """ .rbg Удаляет фон на изображении. """
     input_str = remob.pattern_match.group(1)
     message_id = remob.message.id
     if remob.reply_to_msg_id:
         message_id = remob.reply_to_msg_id
         reply_message = await remob.get_reply_message()
-        await remob.edit("`З а г р у з к а..`")
+        await remob.edit("`Обработка..`")
         try:
             if isinstance(
                     reply_message.media, MessageMediaPhoto
             ) or "image" in reply_message.media.document.mime_type.split('/'):
                 downloaded_file_name = await remob.client.download_media(
                     reply_message, TEMP_DOWNLOAD_DIRECTORY)
-                await remob.edit("`Removing background from this image..`")
+                await remob.edit("`Удаляем фон..`")
                 output_file_name = await ReTrieveFile(downloaded_file_name)
                 os.remove(downloaded_file_name)
             else:
-                await remob.edit("`How do I remove the background from this ?`"
+                await remob.edit("`Как мне удалить фон из этого?`"
                                  )
         except Exception as e:
             await remob.edit(str(e))
             return
     elif input_str:
         await remob.edit(
-            f"`Removing background from online image hosted at`\n{input_str}")
+            f"`Удаление фона из онлайн-изображения, размещенного на`\n{input_str}")
         output_file_name = await ReTrieveURL(input_str)
     else:
-        await remob.edit("`I need something to remove the background from.`")
+        await remob.edit("`Мне нужно что-то, чтобы удалить фон из.`")
         return
     contentType = output_file_name.headers.get("content-type")
     if "image" in contentType:
@@ -57,12 +53,12 @@ async def kbg(remob):
             await remob.client.send_file(
                 remob.chat_id,
                 remove_bg_image,
-                caption="Background removed using remove.bg",
+                caption="Фон удален",
                 force_document=True,
                 reply_to=message_id)
             await remob.delete()
     else:
-        await remob.edit("**Error (Invalid API key, I guess ?)**\n`{}`".format(
+        await remob.edit("**Ошибка (Неверный API ключ)**\n`{}`".format(
             output_file_name.content.decode("UTF-8")))
 
 
@@ -70,7 +66,7 @@ async def kbg(remob):
 # with the name provided.
 async def ReTrieveFile(input_file_name):
     headers = {
-        "X-API-Key": "ybNdcouHbSEW7zQnWXeK9kgg",
+        "X-API-Key": 'ybNdcouHbSEW7zQnWXeK9kgg',
     }
     files = {
         "image_file": (input_file_name, open(input_file_name, "rb")),
@@ -85,7 +81,7 @@ async def ReTrieveFile(input_file_name):
 
 async def ReTrieveURL(input_url):
     headers = {
-        "X-API-Key": "ybNdcouHbSEW7zQnWXeK9kgg",
+        "X-API-Key": 'ybNdcouHbSEW7zQnWXeK9kgg',
     }
     data = {"image_url": input_url}
     r = requests.post("https://api.remove.bg/v1.0/removebg",
@@ -98,6 +94,6 @@ async def ReTrieveURL(input_url):
 
 CMD_HELP.update({
     "rbg":
-    ".rbg <Link to Image> or reply to any image (Warning: does not work on stickers.)\
-\nUsage: Removes the background of images, using remove.bg API"
+    ".rbg <ссылока на изображение> или ответ на изображение (Внимание: не работает на стикерах.)\
+\nИспользование: Удаляет фон изображений, используя API remove.bg."
 })
