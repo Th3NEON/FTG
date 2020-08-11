2db40653dd596f9d7a24eea81c9ff379fbd46cf9
begi717 <61600291+begi717@users.noreply.github.com>
2020-08-11 18:00:49 +0500

    Update Полиция.py

--- Полиция.py
+++ Полиция.py
@@ -4,7 +4,7 @@ import os
 import sys
 
 
-@borg.on(events.NewMessage(pattern=r"\.pol", outgoing=True))
+@borg.on(events.NewMessage(pattern=r"\.police", outgoing=True))
 async def _(event):
     if event.fwd_from:
         return
